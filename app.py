ad19350b60b1f833e374f7ab72a67d9b6710a679
Muhammad Mahfuzur Rahman <mahfuzur993.rahman@gmail.com>
2025-07-22 22:53:11 +0100

    updated

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def predict_datapoint():
     
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)    
+    app.run(host="0.0.0.0", port=8181)    
 
 
